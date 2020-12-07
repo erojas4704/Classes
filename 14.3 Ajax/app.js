409af91517c59899bc794a7631a96fcd62e42688
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2020-12-06 23:56:46 -0500

    14.3 Ajax

---
+++ 14.3 Ajax/app.js
@@ -0,0 +1,55 @@
+const onFormSubmit = e => {
+    e.preventDefault();
+
+    let form = {};
+
+    $(e.target).find("input").each((index, node) => {
+        form[node.name] = $(node).val();
+        //It's nice to spam search //$(node).val("");
+    });
+
+    getGif(form.term);
+}
+
+const deleteAll = e => $(".image").remove()
+
+const getGif = async term => {
+    if(term.length < 1){
+        alert("type something in, you dipstick");
+        return;
+    }
+
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search`, {
+        params: {
+            q: term,
+            api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
+        }
+    }).catch( err => {
+        console.log(err);
+        console.error("Could not retrieve the HILARIOUS THE LE MEME");
+        alert("How did you @#$% this up?");
+    })
+
+    let imgArray = response.data.data;
+    let randomIndex = (Math.random() * imgArray.length) | 0;
+    renderImage(response.data.data[randomIndex]);
+}
+
+const renderImage = imageData => {
+    let imgURL = imageData.images.original.url;
+
+    let $new = $(document.getElementById("template-image").content.cloneNode(true));
+    let $img = $new.find("img");
+
+    $new.find("h5").html(imageData.title);
+
+    $("#images").append($new);
+    $img.attr("src", imgURL);
+
+    return $new;
+}
+
+$(() => {
+    $('#the_form').on('submit', onFormSubmit);
+    $('#btn_delete').click(deleteAll);
+});
