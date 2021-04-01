46e4100dc134a476b1c0b9559c66140bed00f129
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-04-01 18:33:33 -0400

    19.3

--- 19.3 Flask Tools/flask-survey/app.py
+++ 19.3 Flask Tools/flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from surveys import *
 
 print("running")
@@ -18,17 +18,25 @@ def index():
         
     return render_template("index.j2", surveys=surveys.values())
 
-    
+
 @app.route('/survey/<survey_title>')
 def survey(survey_title):
-    print(f"TItle of survey is {survey_title}")
-    survey = [survey for survey in surveys.values() if survey.title == survey_title][0]
-    print(survey.title)
-    print(f"Got survey {survey}")
+    print(f'./survey/{survey_title}/questions/0')
+    return redirect(f'{survey_title}/questions/0')
 
-    return render_template("survey.j2", survey=survey)
     
+@app.route('/survey/<survey_title>/questions/<index>')
+def question(survey_title, index):
+    print('do or die')
+    survey = [survey for survey in surveys.values() if survey.title == survey_title][0]
+    print(survey)
+    question = survey.questions[int(index)]
 
-@app.route('/question/<index>')
-def question(index):
-    return render_template("question.j2", question=question)
+    print(question)
+    return render_template("question.j2", question = question)
+
+#@app.route('/survey/<survey_title>')
+#def survey(survey_title):
+#    survey = [survey for survey in surveys.values() if survey.title == survey_title][0]
+#
+#    return render_template("survey.j2", survey=survey)
