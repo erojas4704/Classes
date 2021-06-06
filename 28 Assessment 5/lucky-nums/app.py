5663dd8b5cc16df5c342a6591f2c4e4dd0c0456b
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-06-06 18:51:16 -0400

    28 - Assessment 5

--- 28 Assessment 5/lucky-nums/app.py
+++ 28 Assessment 5/lucky-nums/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from math import isnan
 from datetime import date
+import requests
+import random
 
 app = Flask(__name__)
 
@@ -13,34 +15,65 @@ def homepage():
 
 @app.route("/api/get-lucky-num", methods=["POST"])
 def get_lucky_num():
-    name = request.form.get('name')
+    data = request.json
     errors = {}
 
-    if len( name ) < 1 or name is None:
-        errors["name"] = "This field is required."
+    year = validate_year(data.get('year'), errors)
+    name = validate_name(data.get('name'), errors)
+    color = validate_color(data.get('color'), errors)
+    email = validate_email(data.get('email'), errors)
 
-    year = request.form.get('year')
-
-    if isnan( year ):
-        errors["year"] = "Invalid year."
-    
-    elif year < 1900:
-        errors["year"] = "Nobody that old would waste time on this."
-    
-    elif year > date.today() > year:
-        errors["year"] = "Year cannot be in the future."
-        
-    email = request.form.get('email')
-    if len( email ) < 1 or name is None:
-        errors["email"] = "This field is required."
-
-    color =  request.form.get('color')
-    color = color.lower()
-
-    if color != "red" or color != "orange" or color != "blue" or color != "green":
-        errors["color"] = "Invalid value, must be one of: red, green, orange, blue."
-    
     if errors:
-        return jsonify(errors)
+        return jsonify({'errors': errors})
 
-    return jsonify({"test": "SUCCESS"})
+    lucky_num = random.randrange(0, 100)
+
+    resp = requests.get(f"http://numbersapi.com/{year}/year")
+    data = {}
+
+    data["year"] = year
+    data["yearFact"] = resp.text
+    data["number"] = lucky_num
+    data["numberFact"] = requests.get(f"http://numbersapi.com/{lucky_num}/trivia").text
+    #data["number"] = requests.get("http://numbersapi.com/number/trivia")
+    print(data)
+    return jsonify(data)
+
+def validate_year(year, errors):
+    """Checks that year is within reasonable range."""
+    try:
+        year = int(year)
+    except:
+        year = None
+
+    if year is None:
+        errors["year"] = "Invalid year."
+    elif int(year) < 1900:
+        errors["year"] = "Nobody that old would waste time on this."
+    elif int(year) > date.today().year > year:
+        errors["year"] = "Year cannot be in the future."
+    return year
+
+def validate_name(name, errors):
+    """Checks that name exists."""
+    if name is None or len( name ) < 1:
+        errors["name"] = "This field is required."
+    return name
+
+def validate_email(email, errors):
+    """Checks that email exists."""
+    if email is None or len( email ) < 1:
+        errors["email"] = "This field is required."
+    return email
+
+def validate_color(color, errors):
+    """Checks that color exists and is one of the 4 choices, case not sensitive."""
+    if color is None:
+        errors["color"] = "Invalid value, must be one of: red, green, orange, blue."
+    else:
+        color = color.lower()
+
+    if color not in ["red","orange","blue","green"]:
+        errors["color"] = "Invalid value, must be one of: red, green, orange, blue."
+
+    return color
--- 28 Assessment 5/lucky-nums/static/lucky.js
+++ 28 Assessment 5/lucky-nums/static/lucky.js
@@ -1,12 +1,41 @@
 /** processForm: get data from form and make AJAX call to our API. */
 
-function processForm(evt) {
+async function processForm(evt) {
     evt.preventDefault();
+    resetValues();
+
+    resp = await axios.post("/api/get-lucky-num", {
+        name: $("#name").val(),
+        year: $("#year").val(),
+        email: $("#email").val(),
+        color: $("#color").val()
+    });
+
+    console.log(resp);
+    handleResponse(resp.data);
+}
+
+/** resetValues: clear all the dumb shit we put */
+function resetValues(){
+    $(`#name-err`).text("");
+    $(`#year-err`).text("");
+    $(`#email-err`).text("");
+    $(`#color-err`).text("");
 }
 
 /** handleResponse: deal with response from our lucky-num API. */
 
 function handleResponse(resp) {
+    if(resp.errors){
+        let errors = resp.errors;
+        Object.keys(errors).forEach(key => {
+            $(`#${key}-err`).text( errors[key] );
+        });
+    }else{
+        $("#lucky-results").text(`
+        Your lucky number is ${resp.number} (${resp.numberFact}).
+        Your birth year (${resp.year}) fact is ${resp.yearFact}.`);
+    }
 }
 
 
