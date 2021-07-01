df4c90906be50ae4517124eb6f665200ed530748
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-07-01 01:25:57 -0400

    37

---
+++ 37 Cummulative Project 2/helpers/sql.js
@@ -0,0 +1,52 @@
+const { BadRequestError } = require("../expressError");
+
+/**
+ * Converts a JSON object to a SQL command, allowing for partially updating a row in a table with 
+ * an arbitrary list of properties.
+ * @param {*} dataToUpdate The body of data with which we'll be updating our database row with.
+ * @param {*} jsToSql A dictionary to translate names into their SQL counterparts. For example:
+ * firstName to first_name, depending on the schema. If none is specified, it will default to the name of the 
+ * key in the data supplied.
+ * @returns A string with a formatted SQL query for a partial update.
+ */
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  const cols = keys.map((colName, idx) =>
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+/**
+ * Converts a JSON object to a SQL command, allowing for generating a WHERE clause with an arbitrary number of filters.
+ * @param {*} criteria A dictionary containing the properties and values we're checking for.
+ * @param {*} comparators A dictionary of operators we'll be using for comparison. Can be =, LIKE, <, >, and more.
+ * @param {*} jsToSql A dictionary to translate names into their SQL counterparts. For example:
+ * firstName to first_name, depending on the schema. If none is specified, it will default to the name of the 
+ * key in the data supplied.
+ * @returns A string with a formatted SQL query for a partial update.
+ */
+function generateWhereClause(criteria, comparators, jsToSql) {
+  const keys = Object.keys(criteria);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  const filters = keys.map((keyName, i) => {
+    if(!comparators[keyName] || comparators[keyName] === 'ILIKE'){
+      criteria[keyName] = `%${criteria[keyName]}%`; //Encapsulate in % if it's an ILIKE string.
+    }
+
+    return `${jsToSql[keyName] || keyName} ${comparators[keyName] || 'ILIKE'} $${i + 1}`
+  });
+
+  return {
+    whereClause: `WHERE ${filters.join(" AND ")}`,
+    values: Object.values(criteria)
+  }
+}
+module.exports = { sqlForPartialUpdate, generateWhereClause };
---
+++ 37 Cummulative Project 2/models/company.test.js
@@ -0,0 +1,293 @@
+"use strict";
+
+const db = require("../db.js");
+const { BadRequestError, NotFoundError } = require("../expressError");
+const Company = require("./company.js");
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** create */
+
+describe("create", function () {
+  const newCompany = {
+    handle: "new",
+    name: "New",
+    description: "New Description",
+    numEmployees: 1,
+    logoUrl: "http://new.img",
+  };
+
+  test("works", async function () {
+    let company = await Company.create(newCompany);
+    expect(company).toEqual(newCompany);
+
+    const result = await db.query(
+      `SELECT handle, name, description, num_employees, logo_url
+           FROM companies
+           WHERE handle = 'new'`);
+    expect(result.rows).toEqual([
+      {
+        handle: "new",
+        name: "New",
+        description: "New Description",
+        num_employees: 1,
+        logo_url: "http://new.img",
+      },
+    ]);
+  });
+
+  test("bad request with dupe", async function () {
+    try {
+      await Company.create(newCompany);
+      await Company.create(newCompany);
+      fail();
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+});
+
+/************************************** findAll */
+
+describe("findAll", function () {
+  test("works: no filter", async function () {
+    let companies = await Company.findAll();
+    expect(companies).toEqual([
+      {
+        handle: "c1",
+        name: "C1",
+        description: "Desc1",
+        numEmployees: 1,
+        logoUrl: "http://c1.img",
+      },
+      {
+        handle: "c2",
+        name: "C2",
+        description: "Desc2",
+        numEmployees: 2,
+        logoUrl: "http://c2.img",
+      },
+      {
+        handle: "c3",
+        name: "C3",
+        description: "Desc3",
+        numEmployees: 3,
+        logoUrl: "http://c3.img",
+      },
+    ]);
+  });
+});
+
+/************************************** get */
+
+describe("get", function () {
+  test("works", async function () {
+    let company = await Company.get("c1");
+    expect(company).toEqual({
+      handle: "c1",
+      name: "C1",
+      description: "Desc1",
+      numEmployees: 1,
+      logoUrl: "http://c1.img",
+    });
+  });
+
+  test("not found if no such company", async function () {
+    try {
+      await Company.get("nope");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
+
+describe("get by criteria ", () => {
+  test("Make sure there's an error if we pass invalid arguments ", async () => {
+    try {
+      await Company.findByCriteria({ minEmployees: 40, maxEmployees: 20 });
+    } catch (err) {
+      expect(err.message).toEqual("Minimum cannot be higher than max!");
+    }
+  });
+
+  test("Make sure we can find a company given a name", async () => {
+    let companies = await Company.findByCriteria({ name: 'c2' });
+    expect(companies).toEqual([{
+      handle: "c2",
+      name: "C2",
+      description: "Desc2",
+      numEmployees: 2,
+      logoUrl: "http://c2.img",
+    }]);
+  });
+
+  test("Make sure we can find a list of companies with a common name", async () => {
+    let companies = await Company.findByCriteria({ name: 'c' });
+    expect(companies).toEqual([
+      {
+        handle: "c1",
+        name: "C1",
+        description: "Desc1",
+        numEmployees: 1,
+        logoUrl: "http://c1.img",
+      },
+      {
+        handle: "c2",
+        name: "C2",
+        description: "Desc2",
+        numEmployees: 2,
+        logoUrl: "http://c2.img",
+      },
+      {
+        handle: "c3",
+        name: "C3",
+        description: "Desc3",
+        numEmployees: 3,
+        logoUrl: "http://c3.img",
+      },
+    ]);
+  });
+
+  test("Make sure we can filter by name and by number", async () => {
+    let companies = await Company.findByCriteria({ name: 'c2', minEmployees: 0, maxEmployees: 4 });
+    expect(companies).toEqual([
+      {
+        handle: "c2",
+        name: "C2",
+        description: "Desc2",
+        numEmployees: 2,
+        logoUrl: "http://c2.img",
+      }
+    ]);
+  });
+
+  test("Make sure we can narrow down the number of employees properly", async () => {
+    let companies = await Promise.all([
+      Company.findByCriteria({ minEmployees: 2 }),
+      Company.findByCriteria({ maxEmployees: 2 })
+    ]);
+    expect(companies[0]).toEqual([
+      {
+        handle: "c3",
+        name: "C3",
+        description: "Desc3",
+        numEmployees: 3,
+        logoUrl: "http://c3.img",
+      }
+    ]);
+    expect(companies[1]).toEqual([
+      {
+        handle: "c1",
+        name: "C1",
+        description: "Desc1",
+        numEmployees: 1,
+        logoUrl: "http://c1.img",
+      }
+    ]);
+  });
+})
+/************************************** update */
+
+describe("update", function () {
+  const updateData = {
+    name: "New",
+    description: "New Description",
+    numEmployees: 10,
+    logoUrl: "http://new.img",
+  };
+
+  test("works", async function () {
+    let company = await Company.update("c1", updateData);
+    expect(company).toEqual({
+      handle: "c1",
+      ...updateData,
+    });
+
+    const result = await db.query(
+      `SELECT handle, name, description, num_employees, logo_url
+           FROM companies
+           WHERE handle = 'c1'`);
+    expect(result.rows).toEqual([{
+      handle: "c1",
+      name: "New",
+      description: "New Description",
+      num_employees: 10,
+      logo_url: "http://new.img",
+    }]);
+  });
+
+  test("works: null fields", async function () {
+    const updateDataSetNulls = {
+      name: "New",
+      description: "New Description",
+      numEmployees: null,
+      logoUrl: null,
+    };
+
+    let company = await Company.update("c1", updateDataSetNulls);
+    expect(company).toEqual({
+      handle: "c1",
+      ...updateDataSetNulls,
+    });
+
+    const result = await db.query(
+      `SELECT handle, name, description, num_employees, logo_url
+           FROM companies
+           WHERE handle = 'c1'`);
+    expect(result.rows).toEqual([{
+      handle: "c1",
+      name: "New",
+      description: "New Description",
+      num_employees: null,
+      logo_url: null,
+    }]);
+  });
+
+  test("not found if no such company", async function () {
+    try {
+      await Company.update("nope", updateData);
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+
+  test("bad request with no data", async function () {
+    try {
+      await Company.update("c1", {});
+      fail();
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+});
+
+/************************************** remove */
+
+describe("remove", function () {
+  test("works", async function () {
+    await Company.remove("c1");
+    const res = await db.query(
+      "SELECT handle FROM companies WHERE handle='c1'");
+    expect(res.rows.length).toEqual(0);
+  });
+
+  test("not found if no such company", async function () {
+    try {
+      await Company.remove("nope");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
